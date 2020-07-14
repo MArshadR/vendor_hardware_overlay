fcb44dc5226ef5e75300246fb9d60ea3d434ef5e
MArshadR <mohamedarshadr@gmail.com>
2020-07-14 21:13:08 +0800

    Add overlay for Xiaomi Mi 10

--- overlay.mk
+++ overlay.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote6pro-systemui \
 	treble-overlay-xiaomi-redminote7 \
 	treble-overlay-xiaomi-redminote8pro \
+        treble-overlay-xiaomi-mi10 \
 	treble-overlay-xiaomi-redmis2 \
 
 
